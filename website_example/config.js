b2ae346f25d109dcae5fafc9ee370ec3fc626fa1
DevOpsRalf <ralf@dreamitsystems.com>
2018-06-25 02:45:23 +0000

    Initial commit

---
+++ website_example/config.js
@@ -0,0 +1,15 @@
+var api = "";
+
+var api_blockexplorer = "";
+
+var email = "";
+var telegram = "";
+var discord = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "";
+var transactionExplorer = "";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
